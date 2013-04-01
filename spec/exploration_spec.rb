5b6070bde389a9707504bf6a90bdedd7c9bb834b
Philipp Brendel <waldrumpus@gmail.com>
2013-04-01 13:10:02 +0200

    add: Exploration specs

--- spec/exploration_spec.rb
+++ spec/exploration_spec.rb
@@ -5,7 +5,7 @@ describe Exploration do
     h = double
     h.stub(:senses_glitter?).and_return(true)
     e = Exploration.new
-    e.advance(h).should eq(:shoot)
+    e.advance(h).should eq(:grab)
   end
   
   it "climbs if agent has the gold and is on start" do
@@ -14,60 +14,17 @@ describe Exploration do
     h.stub(:has_gold?).and_return(true)
     h.stub(:on_start?).and_return(true)
     e = Exploration.new
-    e.advance(h).should eq(:shoot)
+    e.advance(h).should eq(:climb)
   end
   
-  # it "returns to start if gold is found" do
-  #   r = double
-  #   r.stub(:return_state=)
-  #   r.should_receive(:advance)
-  #   Expedition = double
-  #   Expedition.should_receive(:new).and_return(r)
-  # 
-  #   h = double
-  #   h.stub(:senses_glitter?).and_return(false)
-  #   h.stub(:has_gold?).and_return(true)
-  #   h.stub(:on_start?).and_return(false)
-  #   h.stub(:state=)
-  # 
-  #   e = Exploration.new
-  #   e.advance(h)
-  # end
-  # 
-  # it "looks for a safe square" do
-  #   r = double
-  #   r.stub(:return_state=)
-  #   r.should_receive(:advance)
-  #   Expedition = double
-  #   Expedition.should_receive(:new).and_return(r)
-  # 
-  #   h = double
-  #   h.stub(:senses_glitter?).and_return(false)
-  #   h.stub(:has_gold?).and_return(false)
-  #   h.stub(:state=)
-  #   h.should_receive(:get_safe_square).and_return([1, 3])
-  # 
-  #   e = Exploration.new
-  #   e.advance(h)
-  # end
-  # 
-  # it "hunts the wumpus" do
-  #   r = double
-  #   r.stub(:return_state=)
-  #   r.should_receive(:advance)
-  #   Extermination = double
-  #   Extermination.should_receive(:new).and_return(r)
-  # 
-  #   h = double
-  #   h.stub(:senses_glitter?).and_return(false)
-  #   h.stub(:has_gold?).and_return(false)
-  #   h.stub(:state=)
-  #   h.should_receive(:get_safe_square)
-  #   h.should_receive(:wumpus_found?).and_return(true)
-  #   h.should_receive(:wumpus_killed?).and_return(false)
-  #   h.should_receive(:wumpus_location).and_return([0, 0])
-  # 
-  #   e = Exploration.new
-  #   e.advance(h)
-  # end
+  it "climbs if there are no more options left and the agent is on start" do
+    h = double(:senses_glitter? => false,
+               :has_gold? => false,
+               :get_safe_square => nil,
+               :wumpus_found? => false,
+               :get_dangerous_square => nil,
+               :on_start? => true)
+    e = Exploration.new
+    e.advance(h).should eq :climb
+  end
 end
