bb5fb0508f048ba6dc3c15fd846387f6580da4dd
Philipp Brendel <waldrumpus@gmail.com>
2013-04-01 13:08:28 +0200

    fix: Exploration issues appropriate actions
    All actions have been replaced by :shoot during a careless
    find-and-replace.

--- lib/bronco/states/exploration.rb
+++ lib/bronco/states/exploration.rb
@@ -3,8 +3,8 @@ require 'bronco/states/extermination'
 
 class Exploration
   def advance(agent)
-    return :shoot if agent.senses_glitter?
-    return :shoot if agent.has_gold? && agent.on_start?
+    return :grab if agent.senses_glitter?
+    return :climb if agent.has_gold? && agent.on_start?
     
     return transition(Expedition.new, agent) if agent.has_gold?
       
@@ -16,7 +16,7 @@ class Exploration
     dangerous_square = agent.get_dangerous_square
     return transition(Expedition.new(dangerous_square), agent) if dangerous_square
     
-    return :shoot if agent.on_start?
+      return :climb 
     
     return transition(Expedition.new(agent.start_location), agent)
   end
